7478c7923c11f18f1e5a8b19916b299d33402900
Francofrt1 <80823509+Francofrt1@users.noreply.github.com>
2025-05-29 15:34:01 -0300

    Refactor GenerateForest method in GameManager
    Significantly updated the `GenerateForest` method to include error handling with a `try-catch` block. Improved the logic for generating tree positions by checking if they are on grass and sufficiently spaced apart before instantiation. Introduced a new `SpawnTree` method for better code organization and readability. Enhanced the loop structure with boolean flags for improved clarity and maintainability.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,22 +58,37 @@ public class GameManager : BaseNetworkBehaviour
     //TODO fix trees not spawning
     public void GenerateForest()
     {
-        int i = 0;
-        Terrain terrain = Terrain.activeTerrain;
-        int grassIndex = 0;
-        float minDistanceTrees = 3f;
-        GameObject treeContainer = GameObject.Find("TreeContainer");
-        while (i < 3800)
+        try
         {
-            Vector3 randomPos = GetRandomPositionOnTerrain(terrain);
-            if (IsOnGrass(randomPos, terrain, grassIndex) && IsPositionFarEnough(randomPos, minDistanceTrees))
+            int i = 0;
+            Terrain terrain = Terrain.activeTerrain;
+            int grassIndex = 0;
+            float minDistanceTrees = 3f;
+            GameObject treeContainer = GameObject.Find("TreeContainer");
+            while (i < 3800)
             {
-                int randomTree = UnityEngine.Random.Range(0, treePrefab.Count - 1);
-                var tree = Instantiate(treePrefab[randomTree], randomPos, Quaternion.identity, terrain.transform);
-                //ServerManager.Spawn(tree);
-                i++;
+                Vector3 randomPos = GetRandomPositionOnTerrain(terrain);
+                bool isOnGrass = IsOnGrass(randomPos, terrain, grassIndex);
+                bool isFarEnough = IsPositionFarEnough(randomPos, minDistanceTrees);
+                if (isOnGrass && isFarEnough)
+                {
+                    int treeCount = treePrefab.Count - 1;
+                    int randomTreeIndex = UnityEngine.Random.Range(0, treeCount);
+                    SpawnTree(randomTreeIndex, randomPos, terrain.transform);
+                    i++;
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Debug.Log($"GenerateForest error {ex.Message}");
+        }
+    }
+
+    public void SpawnTree(int randomTreeIndex, Vector3 randomPos, Transform transform)
+    {
+        var tree = Instantiate(treePrefab[randomTreeIndex], randomPos, Quaternion.identity, transform);
+        ServerManager.Spawn(tree);
     }
 
     Vector3 GetRandomPositionOnTerrain(Terrain terrain)
