c7f18e18e5dc3287dcd5b2bd3cef9d764a071aef
Francofrt1 <80823509+Francofrt1@users.noreply.github.com>
2025-06-15 23:00:56 -0300

    Refactor player prefab and scripts for improved functionality
    Updated `PlayerMultiplayer.prefab` to include new components and properties, changing `m_IsActive` to `0` to deactivate the object. Reorganized `gameObjectsToEnable` and `componentsToEnable` for better management.
    
    In `PlayerClient.cs`, replaced old arrays with new ones for flexibility and updated `Rpc_ToggleController` to reflect these changes.
    
    Enhanced `PlayerPresenter.cs` by introducing serialized fields for component references and modifying the `Awake` method for clarity. Added error handling for critical components to improve robustness.

--- Assets/Scripts/Multiplayer/PlayerClient.cs
+++ Assets/Scripts/Multiplayer/PlayerClient.cs
@@ -5,6 +5,7 @@ using Multiplayer.PopupSystem;
 using Multiplayer.Utils;
 using Steamworks;
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -32,8 +33,10 @@ namespace Multiplayer.PlayerSystem
         public readonly SyncVar<PlayerInfoData> PlayerInfo = new SyncVar<PlayerInfoData>();
         public readonly SyncVar<bool> IsReady = new SyncVar<bool>();
 
-        [SerializeField] private GameObject[] componentsToEnable;
-        [SerializeField] private GameObject[] componentsToDisable;
+        [SerializeField] private GameObject[] gameObjectsToEnable;
+        [SerializeField] private Behaviour[] componentsToEnable;
+        [SerializeField] private GameObject inGameContainer;
+        [SerializeField] private GameObject inMenuContainer;
 
         [Header("Party NameTag")] 
         [SerializeField] private TMP_Text _usernameText;
@@ -73,19 +76,21 @@ namespace Multiplayer.PlayerSystem
         [ObserversRpc]
         public void Rpc_ToggleController(bool value)
         {
-            
-            if(!IsOwner) return;
+            inMenuContainer.SetActive(!value);
+            inGameContainer.SetActive(value);
+
+            if (!IsOwner) return;
 
             Cursor.lockState = CursorLockMode.Locked;
-            
-            foreach (var component in componentsToEnable)
+
+            foreach (var gameObject in gameObjectsToEnable)
             {
-                component.SetActive(value);
+                gameObject.SetActive(value);
             }
 
-            foreach (var component in componentsToDisable) 
+            foreach (var component in componentsToEnable)
             {
-                component.SetActive(!value);
+                component.enabled = value;
             }
         }
 
